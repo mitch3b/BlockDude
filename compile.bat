251a0566bdcd4f901158d430889839855b128325
mitch3a <mitchkendall@gmail.com>
2018-06-08 17:48:35 -0400

    got windows able to build a rom

--- compile.bat
+++ compile.bat
@@ -1,20 +1,20 @@
 @echo off
 
-set name="lesson10"
+set name="BlockDude"
 
 set path=%path%;..\bin\
 
-set CC65_HOME=..\
+set CC65_HOME=C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin
 
-cc65 -Oi %name%.c --add-source
-ca65 reset.s
-ca65 %name%.s
-ca65 asm4c.s
+C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin\cc65 -Oi %name%.c --add-source
+C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin\ca65 reset.s
+C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin\ca65 %name%.s
+C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin\ca65 asm4c.s
 
-ld65 -C nes.cfg -o %name%.nes reset.o %name%.o asm4c.o nes.lib
+C:\Users\mitch3a\Downloads\cc65-snapshot-win32\bin\ld65 -C nes.cfg -o %name%.nes reset.o %name%.o asm4c.o "C:\Users\mitch3a\Downloads\cc65-snapshot-win32\lib\nes.lib"
 
 del *.o
 
 pause
 
-%name%.nes
+
